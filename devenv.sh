3aeaa9864b02ccb470fbe99cd6c25e171ddc7b9a
George Ma <z.george.ma@gmail.com>
2014-12-27 15:36:31 +1100

    Add Vagrantfile

--- devenv.sh
+++
@@ -1,1 +0,0 @@
-docker run -v /var/run/docker.sock:/run/docker.sock -v /usr/local/bin/docker:/bin/docker -v $(pwd):/project -it chainone/nodejs-mongodb-git /bin/bash
--- gulpfile.js
+++ gulpfile.js
@@ -2,42 +2,38 @@ var gulp = require('gulp');
 var del = require('del');
 var run = require('gulp-run');
 
-gulp.task('setup', ['clean'], function(cb) {
-  gulp.src(['src/*.js', 'src/*.json'])
-      .pipe(gulp.dest('build'));
+gulp.task('copy', ['clean'], function() {
+  return gulp.src(['src/**/*'])
+             .pipe(gulp.dest('build'));
   
-  gulp.src(['src/config/**'])
-      .pipe(gulp.dest('build/config'));
-
-  gulp.src(['src/test/**'])
-      .pipe(gulp.dest('build/test'));
-
-  cb();
 });
 
 gulp.task('clean', function(cb) {
   del(['build', 'output'], cb);
 });
 
-gulp.task('export', ['setup'], function(cb) {
-  run('cd build && npm install && node export').exec(cb);
+gulp.task('export', ['copy'], function(cb) {
+  run('npm install && node export', {cwd:'build'}).exec(cb);
 });
 
 gulp.task('test', ['export'], function(cb) {
-  run('cd build && npm run test').exec(cb);
+  run('npm run test', {cwd:'build'}).exec(cb);
 });
 
 gulp.task('build', ['test'], function(cb) {
   gulp.src(['build/*.js', 'build/*.json', 'build/postcodes.sqlite'])
-      .pipe(gulp.dest('output'));
+      .pipe(gulp.dest('output'))
+      .on('end', function() {
+        gulp.src(['src/config/production.json'])
+            .pipe(gulp.dest('output/config'))
+            .on('end', function() {
+              gulp.src(['Dockerfile'])
+                  .pipe(gulp.dest('output/'))
+                  .on('end', function() {
+                    run('docker build -t aulocapi . && docker save aulocapi | gzip > ../docker.tar.gz', {cwd:'output'}).exec(cb);
+                  });
+            });
+      });
   
-  gulp.src(['src/config/production.json'])
-      .pipe(gulp.dest('output/config'));
-
-  gulp.src(['Dockerfile'])
-      .pipe(gulp.dest('output/'));
-
-  run('docker build -t aulocapi . && docker save aulocapi | gzip > docker.tar.gz').exec(cb);
-
 });
 
